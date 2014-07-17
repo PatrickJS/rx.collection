d0fcad2675ff40db1460e661e1c71c03e4eceb3c
gdi2290 <github@gdi2290.com>
2014-07-16 21:21:49 -0700

    init example index

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
-angular.module('rx.colection', [
-])
+;(function(global, Rx, _) {
+  'use strict';
+
+  // Observable
+  var inherit = function(protoProps, staticProps) {
+    var parent = this;
+    var child;
+
+    // The constructor function for the new subclass is either defined by you
+    // (the "constructor" property in your `extend` definition), or defaulted
+    // by us to simply call the parent's constructor.
+    if (protoProps && _.has(protoProps, 'constructor')) {
+      child = protoProps.constructor;
+    } else {
+      child = function(){ return parent.apply(this, arguments); };
+    }
+
+    // Add static properties to the constructor function, if supplied.
+    _.extend(child, parent, staticProps);
+
+    // Set the prototype chain to inherit from `parent`, without calling
+    // `parent`'s constructor function.
+    var Surrogate = function(){ this.constructor = child; };
+    Surrogate.prototype = parent.prototype;
+    /* jshint ignore:start */
+    child.prototype = new Surrogate;
+    /* jshint ignore:end */
+
+    // Add prototype properties (instance properties) to the subclass,
+    // if supplied.
+    if (protoProps) _.extend(child.prototype, protoProps);
+
+    // Set a convenience property in case the parent's prototype is needed
+    // later.
+    child.__super__ = parent.prototype;
+
+    return child;
+  };
+
+}(window, window.Rx, window._));
