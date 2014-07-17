1905a7833d1eb26a93f6c0125597a165f20862bb
gdi2290 <github@gdi2290.com>
2014-07-16 22:08:58 -0700

    fork index to es3 and es6

---
+++ dist/rxc.es6.js
@@ -0,0 +1,5 @@
+"use strict";
+var Model = function Model() {};
+($traceurRuntime.createClass)(Model, {}, {});
+var Collection = function Collection() {};
+($traceurRuntime.createClass)(Collection, {}, {});
